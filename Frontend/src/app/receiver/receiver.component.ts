8657a37de181386ce7a4cc032b8adc85cd7f918c
roshini <jeevanaroshini@gmail.com>
2022-09-24 21:00:53 +0000

    added by roshini

--- Frontend/src/app/receiver/receiver.component.ts
+++ Frontend/src/app/receiver/receiver.component.ts
@@ -22,6 +22,8 @@ export class ReceiverComponent implements OnInit {
   }
   userdata:any
   cusdata:any
+  balance:Number=0;
+  overdraft:string="";
 
   getCustomerDetails(){
     let response=this.dbshttps.get("http://localhost:8080/sender?username="+this.username);
@@ -29,23 +31,27 @@ export class ReceiverComponent implements OnInit {
       this.userdata=dbsdata;
      console.log(this.userdata);
      this.userName=this.userdata[0].custName;
-      
+     this.balance=this.userdata[0].balance;
+     this.overdraft=this.userdata[0].overDraft;
     });
 
     response=this.dbshttps.get("http://localhost:8080/sender?username="+localStorage.getItem);
     response.subscribe((dbsdata)=>{
       this.cusdata=dbsdata;
      console.log(this.cusdata);
-     this.userName=this.cusdata[0].custName;
+    //  this.userName=this.cusdata[0].custName;
       
     });
+    
   }
   
   makeATransaction(){
     let response=this.dbshttps.get("localhost:8080/receiver?cusId="+this.cusdata[0].cusId+"&amount="+this.amount+
     "&cusName="+this.cusdata[0].cusName+"&recId="+this.userdata[0].recId+"&recName="+this.userdata[0].recName+"&status=true");
     
-  
+    response=this.dbshttps.get("localhost:8080/updateBalance?uname="+this.cusdata[0].username+"&custname="+this.cusdata[0].cusName+"&bal="+this.cusdata[0].balance+
+    "&overdraft="+this.cusdata[0].overDraft+"&amount="+this.amount);
+    
   }
 
 }
