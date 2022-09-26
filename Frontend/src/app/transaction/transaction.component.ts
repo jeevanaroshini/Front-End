48139c6d573c9df2ad40d99276834cebc83ee211
Shariq <shariq.mohammed01@gmail.com>
2022-09-26 06:30:44 +0000

    Added by Shariq

--- Frontend/src/app/transaction/transaction.component.ts
+++ Frontend/src/app/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transaction',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+ goHome(){
+  this.router.navigate(['login'])
+ }
+ goSender(){
+  this.router.navigate(['sender'])
+ }
+ goReciever(){
+  this.router.navigate(['receiver'])
+ }
  
   
 }
